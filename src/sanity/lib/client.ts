65db23066b94fdc6f4be846a02950833c28c541f
azmataliakbar <azmat36@yahoo.com>
2025-01-23 00:45:13 +0500

    fixing evv code issue

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,9 +3,9 @@ import { createClient } from 'next-sanity'
 
 
 export const client = createClient({
-  projectId: '1t887iwj',
-  dataset: 'production',
-  apiVersion: '2021-08-31',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  token:'skBaVZFWIgkCRIVRBj5FHi7qjNeTAVlV5GLOLPKJGabN7mEXyxdrcGCLvkiQHLYcMdB6kHKtK8N8rUsOY9ZClsxQ5vVpbfw6GeTat5xSbzqvMuEoVgg1Go37Cv9IMXzYBarGdscAoecFE5JkvYEUzy0fUDWk5i690fpaG59N2fqXbTpSHNfr'
+  token:process.env.SANITY_API_TOKEN, // Add your Sanity token here
 })
